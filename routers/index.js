027827e51e9f261bac16013f0717300a030305f7
IMENDOZA <imendoza@unab.edu.co>
2023-03-16 19:38:53 -0500

    Instalacion Container Postgres

--- routers/index.js
+++
@@ -1,9 +0,0 @@
-const productsRouter = require('./products.router');
-const usersRouter = require('./users.router');
-
-function routerApi(app){
-    app.use('/products',productsRouter);
-    app.use('/users',usersRouter);
-}
-
-module.exports = routerApi;
